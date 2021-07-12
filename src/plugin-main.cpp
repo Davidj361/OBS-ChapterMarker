4027ec7675e02db3f393d958ee4e8ac14c310455
Davidj361 <david.j.361@gmail.com>
2021-07-12 10:40:41 -0400

    Adjusted CMake to include avutil, can include avutil headers now

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -19,6 +19,11 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <obs-module.h>
 #include <obs-frontend-api.h>
 #include <obs.h>
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavutil/dict.h>
+}
+
 #include "plugin-macros.generated.h"
 #include "plugin-main.hpp"
 
